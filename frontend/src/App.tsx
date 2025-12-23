5d248bd454e59b62e80fba24ab1f50ec51809b62
herve <rztherve@gmail.com>
2025-12-23 18:44:16 +0300

    page login par défaut

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" replace /> } />
+        <Route path="/" element={<Navigate to="/login" replace /> } />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" 
